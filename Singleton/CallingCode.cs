8aa83bc1d19cfa8ba180de1d01ac818b16719dd6
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2023-08-18 08:08:25 +0100

    Added thread-safe singleton implementation

---
+++ Singleton/CallingCode.cs
@@ -0,0 +1,15 @@
+namespace Singleton;
+
+public class CallingCode
+{
+    public static void Main()
+    {
+        var instance = Singleton.Instance;
+        var g1 = instance.Guid;
+
+        var instance2 = Singleton.Instance;
+        var g2 = instance2.Guid;
+
+        Console.WriteLine(g1 == g2);
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton;
+
+public sealed class Singleton
+{
+    private static readonly object _lock = new();
+    
+    private static Singleton _instance;
+
+    private Singleton() => Guid = Guid.NewGuid();
+    
+    public Guid Guid { get; }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                lock (_lock)
+                {
+                    _instance ??= new();
+                }
+            }
+            
+            return _instance;
+        }
+    }
+}
